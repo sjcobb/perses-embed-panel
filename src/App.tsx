6ddd6025994e70f315eefb6717b07817e3010d91
Steven Cobb <steven@chronosphere.io>
2025-05-05 11:32:52 -0400

    add fake datasource, no data panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  ChartsProvider,
+  generateChartsTheme,
+  getTheme,
+  SnackbarProvider,
+} from "@perses-dev/components";
+import {
+  DataQueriesProvider,
+  dynamicImportPluginLoader,
+  PluginRegistry,
+  TimeRangeProvider,
+} from "@perses-dev/plugin-system";
+import type { PluginLoader } from "@perses-dev/plugin-system";
+import { Panel } from "@perses-dev/dashboards";
+import { Box, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  DatasourceStoreProvider,
+  VariableProvider,
+} from "@perses-dev/dashboards";
+import * as prometheusPlugin from "@perses-dev/prometheus-plugin";
+import * as timeseriesChartPlugin from "@perses-dev/timeseries-chart-plugin";
+import { fakeDatasourceApi, fakeDashboard } from "./model";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const muiTheme = getTheme("light");
+  const chartsTheme = generateChartsTheme(muiTheme, {});
 
+  const pluginLoader: PluginLoader = dynamicImportPluginLoader([
+    {
+      resource: prometheusPlugin.getPluginModule(),
+      importPlugin: () => Promise.resolve(prometheusPlugin),
+    },
+    {
+      resource: timeseriesChartPlugin.getPluginModule(),
+      importPlugin: () => Promise.resolve(timeseriesChartPlugin),
+    },
+  ]);
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 0,
+      },
+    },
+  });
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={muiTheme}>
+      <ChartsProvider chartsTheme={chartsTheme}>
+        <SnackbarProvider
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          variant="default"
+          content=""
+        >
+          <PluginRegistry
+            pluginLoader={pluginLoader}
+            defaultPluginKinds={{
+              Panel: "TimeSeriesChart",
+              TimeSeriesQuery: "PrometheusTimeSeriesQuery",
+            }}
+          >
+            <QueryClientProvider client={queryClient}>
+              <TimeRangeProvider
+                refreshInterval="0s"
+                timeRange={{ pastDuration: "30m" }}
+              >
+                <VariableProvider>
+                  <DatasourceStoreProvider
+                    dashboardResource={fakeDashboard}
+                    datasourceApi={fakeDatasourceApi}
+                  >
+                    <Box sx={{ width: 600, height: 300 }}>
+                      <DataQueriesProvider
+                        definitions={[]}
+                        // definitions={[
+                        //   {
+                        //     kind: "PrometheusTimeSeriesQuery",
+                        //     // spec: { query: `up{job="prometheus"}` },
+                        //     spec: { query: `1` },
+                        //   },
+                        // ]}
+                      >
+                        <Panel
+                          panelOptions={{
+                            hideHeader: true,
+                          }}
+                          definition={{
+                            kind: "Panel",
+                            spec: {
+                              // queries: queries,
+                              // queries: [],
+                              display: { name: "Example" },
+                              plugin: {
+                                kind: "TimeSeriesChart",
+                                spec: {
+                                  legend: {
+                                    position: "bottom",
+                                    size: "medium",
+                                  },
+                                },
+                              },
+                            },
+                          }}
+                        />
+                      </DataQueriesProvider>
+                    </Box>
+                  </DatasourceStoreProvider>
+                </VariableProvider>
+              </TimeRangeProvider>
+            </QueryClientProvider>
+          </PluginRegistry>
+        </SnackbarProvider>
+      </ChartsProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/model.ts
@@ -0,0 +1,50 @@
+import type {
+  DashboardResource,
+  GlobalDatasourceResource,
+  DatasourceResource,
+} from "@perses-dev/core";
+import type { DatasourceApi } from "@perses-dev/dashboards";
+
+const fakeDatasource: GlobalDatasourceResource = {
+  kind: "GlobalDatasource",
+  metadata: { name: "hello" },
+  spec: {
+    default: true,
+    plugin: {
+      kind: "PrometheusDatasource",
+      spec: {
+        directUrl: "https://prometheus.demo.do.prometheus.io",
+      },
+    },
+  },
+};
+
+class DatasourceApiImpl implements DatasourceApi {
+  getDatasource(): Promise<DatasourceResource | undefined> {
+    return Promise.resolve(undefined);
+  }
+
+  getGlobalDatasource(): Promise<GlobalDatasourceResource | undefined> {
+    return Promise.resolve(fakeDatasource);
+  }
+
+  listDatasources(): Promise<DatasourceResource[]> {
+    return Promise.resolve([]);
+  }
+
+  listGlobalDatasources(): Promise<GlobalDatasourceResource[]> {
+    return Promise.resolve([fakeDatasource]);
+  }
+
+  buildProxyUrl(): string {
+    return "/prometheus";
+  }
+}
+
+export const fakeDatasourceApi = new DatasourceApiImpl();
+
+export const fakeDashboard = {
+  kind: "Dashboard",
+  metadata: {},
+  spec: {},
+} as DashboardResource;
