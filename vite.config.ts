077e0992d7260b43f23841620a98e6fce911b722
Steven Cobb <steven@chronosphere.io>
2025-05-05 12:08:17 -0400

    change base in vite.config to perses-embed-panel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/perses-embed-panel",
   define: {
     // Workaround for `global is not defined` error
     // https://github.com/vitejs/vite/discussions/5912#discussioncomment-2908994
